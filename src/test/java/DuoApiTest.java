842bccf97d3306d99a38f35efd78e37e8d0316ef
Latiif <llusx@outlook.com>
2019-08-21 21:36:08 +0200

    Add Auth tests

--- src/test/java/DuoApiTest.java
+++ src/test/java/DuoApiTest.java
@@ -1,4 +1,5 @@
 import com.latiif.duoapi.DuoApi;
+import com.latiif.duoapi.DuoRequest;
 import org.junit.Test;
 
 import static junit.framework.TestCase.*;
@@ -7,15 +8,21 @@ public class DuoApiTest {
     @Test
     public void authTestFail() {
         // Testing with wrong password
-        DuoApi duoApi = new DuoApi("llusx", "wrongpassword");
-        // User shouldn't be logged in
-        assertFalse(duoApi.getIsLoggedIn());
+        try {
+            DuoApi duoApi = new DuoApi(new DuoRequest(), "llusx", "wrongpassword");
+        }
+        catch (IllegalArgumentException e){
+            // User shouldn't be logged in
+            assertTrue(true);
+            return;
+        }
+        assertTrue(false);
     }
 
     @Test
     public void authTestSuccess(){
         // Testing with correct password
-        DuoApi duoApi = new DuoApi("llusx", "London12");
+        DuoApi duoApi = new DuoApi(new DuoRequestMockup(),"llusx", "correctpassword");
         // User should be logged in
         assertTrue(duoApi.getIsLoggedIn());
     }
--- src/test/java/DuoRequestMockup.java
+++ src/test/java/DuoRequestMockup.java
@@ -1,4 +1,32 @@
-package PACKAGE_NAME;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import com.latiif.duoapi.IDuoRequest;
 
-public class DuoRequestMockup {
+import java.util.Map;
+
+public class DuoRequestMockup implements IDuoRequest {
+    @Override
+    public void setUserCredentials(String username, String password) {
+
+    }
+
+    @Override
+    public JsonObject makeRequest(String url, Map<String, String> data) {
+        if (data.containsKey("password") && data.get("password").equals("correctpassword")){
+            JsonObject res = new JsonObject();
+            res.addProperty("KEY","VALUE");
+            return res;
+        }
+        return null;
+    }
+
+    @Override
+    public JsonObject getUserData() {
+        return null;
+    }
+
+    @Override
+    public Map<String, String> getCookies() {
+        return null;
+    }
 }
